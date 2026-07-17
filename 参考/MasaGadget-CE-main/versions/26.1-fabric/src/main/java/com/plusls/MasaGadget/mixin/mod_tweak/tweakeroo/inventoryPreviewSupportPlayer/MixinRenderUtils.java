72f32c159b7211606c17b183731c3750a0cb6f8b
Fenad2 <a2771028928@outlook.com>
2026-07-17 17:53:29 +0800

    Remove accidentally tracked reference files

--- 参考/MasaGadget-CE-main/versions/26.1-fabric/src/main/java/com/plusls/MasaGadget/mixin/mod_tweak/tweakeroo/inventoryPreviewSupportPlayer/MixinRenderUtils.java
+++
@@ -1,8 +0,0 @@
-package com.plusls.MasaGadget.mixin.mod_tweak.tweakeroo.inventoryPreviewSupportPlayer;
-
-import org.spongepowered.asm.mixin.Mixin;
-import top.hendrixshen.magiclib.api.preprocess.DummyClass;
-
-@Mixin(DummyClass.class)
-public abstract class MixinRenderUtils {
-}
--- 参考/malilib-26.2/src/main/java/fi/dy/masa/malilib/render/texture/MaLiLibComplexTexture.java
+++
@@ -1,8 +0,0 @@
-package fi.dy.masa.malilib.render.texture;
-
-import com.mojang.blaze3d.textures.GpuSampler;
-import com.mojang.blaze3d.textures.GpuTextureView;
-
-public record MaLiLibComplexTexture(String name, GpuTextureView texture, GpuSampler sampler)
-{
-}
